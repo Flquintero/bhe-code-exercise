81c9407563b7c5aed963cf5305b8a24e31c81c73
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 14:57:24 -0400

    chore: variable name cleanup

--- nth-prime/nth-prime.ts
+++ nth-prime/nth-prime.ts
@@ -2,7 +2,6 @@ const ListParameters = require('../utils/list-parameters');
 const PrimeNumbers = require('../utils/list-prime-numbers');
 
 export const getNthPrime = (targetNumber: number): number => {
-  const primeListLimit: number =
-    ListParameters.generateListLengthLimit(targetNumber);
+  const primeListLimit: number = ListParameters.generateListLimit(targetNumber);
   return PrimeNumbers.listPrimeNumbers(primeListLimit)[targetNumber];
 };
