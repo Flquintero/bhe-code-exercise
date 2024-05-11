fc7692cfce5163a79c53591ef39df97ac7881df9
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 14:57:45 -0400

    chore: variable name cleanup

--- utils/list-parameters.ts
+++ utils/list-parameters.ts
@@ -1,4 +1,4 @@
-export const generateListLengthLimit = (targetPosition: number): number => {
+export const generateListLimit = (targetPosition: number): number => {
   const startLimit = '10';
   const growthFactor = targetPosition.toString().length + startLimit.length;
   return parseInt(startLimit.padEnd(growthFactor, '0'));
