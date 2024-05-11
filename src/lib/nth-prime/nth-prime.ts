005d1548877a493b7851a1cf55de96c18e2763ba
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 16:08:10 -0400

    fix: path to utils

--- src/lib/nth-prime/nth-prime.ts
+++ src/lib/nth-prime/nth-prime.ts
@@ -1,5 +1,5 @@
-const ListParameters = require('../utils/list-parameters');
-const PrimeNumbers = require('../utils/list-prime-numbers');
+const ListParameters = require('../../utils/list-parameters');
+const PrimeNumbers = require('../../utils/list-prime-numbers');
 
 export const getNthPrime = (targetNumber: number): number => {
   const primeListLimit: number = ListParameters.generateListLimit(targetNumber);
