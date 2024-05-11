496c72b3d57804cb921f85595df109cd687f10dd
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 14:22:56 -0400

    feat: added temporary main module method

--- nth-prime/nth-prime.ts
+++ nth-prime/nth-prime.ts
@@ -1,5 +1,8 @@
-const getNthPrime = (targetNumber: number): number => {
-  return targetNumber;
-};
+const ListParameters = require('../utils/list-parameters');
+const PrimeNumbers = require('../utils/list-prime-numbers');
 
-module.exports = getNthPrime;
+export const getNthPrime = (targetNumber: number): number => {
+  const primeListLimit: number =
+    ListParameters.generateListLengthLimit(targetNumber);
+  return PrimeNumbers.listPrimeNumbers(primeListLimit)[targetNumber];
+};
