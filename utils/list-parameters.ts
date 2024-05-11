e1d0d937925b90c3967d82fef4adedf137650089
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 14:26:42 -0400

    fix: generate limit for list, still needs optimization

--- utils/list-parameters.ts
+++ utils/list-parameters.ts
@@ -1,8 +1,5 @@
-class ListParameters {
-  generateListLengthLimit(targetPosition: number) {
-    const growthFactor = targetPosition.toString().length;
-    return parseInt(`1`.padEnd(growthFactor));
-  }
-}
-
-module.exports = ListParameters;
+export const generateListLengthLimit = (targetPosition: number): number => {
+  const startLimit = '10';
+  const growthFactor = targetPosition.toString().length + startLimit.length;
+  return parseInt(startLimit.padEnd(growthFactor, '0'));
+};
